33c56b78a90840267c747f52ec15f5bae75e4fd2
Administrator <Administrator@aiagent.local>
2026-09-25 01:57:15 +0800

    发布 v217906 消息列表收尾测试版

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.aaiagent"
         minSdk = 26
         targetSdk = 34
-       versionCode = 217905
-       versionName = "20260925-015011"
+       versionCode = 217906
+       versionName = "20260925-015643"
     }
 
     buildTypes {
