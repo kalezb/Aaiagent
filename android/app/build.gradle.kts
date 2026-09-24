3f6fab3a27934f1db5eda1520653f4b9a7e2f436
Administrator <Administrator@aiagent.local>
2026-09-25 02:10:13 +0800

    发布 v217908 隐私图夸赞回复版

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.aaiagent"
         minSdk = 26
         targetSdk = 34
-       versionCode = 217907
-       versionName = "20260925-020300"
+       versionCode = 217908
+       versionName = "20260925-020941"
     }
 
     buildTypes {
